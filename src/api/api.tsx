1a5a588354e3af09b2500630866084bf436cfc62
Akhil Balakrishnan <akhilbkrishnan2000@gmail.com>
2024-05-07 14:30:33 +0530

    Trending recipes api added.

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -4,7 +4,7 @@ import { Location, Meal } from '../interface';
 const base_url = 'https://www.themealdb.com/api/json/v1/1';
 
 export const getSuggestedItemAndArea = async (): Promise<{
-    suggestedItem: Meal;
+    suggestedItem: Meal[];
     areaList: Location[];
 }> => {
     const suggestedItemRequest = await axios.get(`${base_url}/random.php`);
@@ -16,7 +16,19 @@ export const getSuggestedItemAndArea = async (): Promise<{
     ]);
 
     return {
-        suggestedItem: suggestedItemResponse.data.meals[0],
+        suggestedItem: suggestedItemResponse.data.meals,
         areaList: areaListResponse.data.meals,
     };
+};
+
+export const getItemsByLocation = async (location: string): Promise<Meal[]> => {
+    const locationBasedRequest = await axios.get(
+        `${base_url}/filter.php?a=${location}`,
+    );
+    return locationBasedRequest.data.meals;
+};
+
+export const getItemById = async (id: number): Promise<Meal[]> => {
+    const idBasedRequest = await axios.get(`${base_url}/lookup.php?i=${id}`);
+    return idBasedRequest.data.meals;
 };
